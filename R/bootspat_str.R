3f2334b47d66f093936df7545942ad08c8c8085a
Neander Marcel Heming <neanderh@yahoo.com.br>
2023-04-27 12:38:21 -0300

    added a function to adjust the probability of a species to be sampled across the raster, so that the sampled frequency of occurrence of the species is closer to the observed

--- R/bootspat_str.R
+++ R/bootspat_str.R
@@ -1,21 +1,69 @@
+
+#' Adjust probability of sampling based on frequency of occurrences.
+#'
+#' This function is used to adjust the probability of a species to be sampled
+#' across the raster, so that the sampled frequency of occurrence of the
+#' species is closer to the observed
+#'
+#' @param x SpatRaster. A presence-absence SpatRaster.
+#'
+#' @return numeric vector
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' library(SESraster)
+#' library(terra)
+#' # creating random species distributions
+#' f <- system.file("ex/elev.tif", package="terra")
+#' r <- rast(f)
+#' set.seed(510)
+#' r10 <- rast(lapply(1:18,
+#'                 function(i, r, mn, mx){
+#'                   app(r, function(x, t){
+#'                     sapply(x, function(x, t){
+#'                        x<max(t) & x>min(t)
+#'                     }, t=t)
+#'                   }, t=sample(seq(mn, mx), 2))
+#'                 }, r=r, mn=minmax(r)[1]+10, mx=minmax(r)[2]-10))
+#'
+#' names(r10) <- paste("sp", 1:nlyr(r10))
+#' fr2prob(r10)
+#' # raw frequencies
+#' subset(terra::freq(r10), value==1)[,"count"]
+#' }
+fr2prob <- function(x){
+  value <- NULL
+  fr <- subset(terra::freq(x), value==1)[,"count"]
+  all <- unlist(terra::global(x[[1]], function(x)sum(!is.na(x), na.rm=T)))
+
+  p <- fr/all
+  pin <- sapply(seq_along(p),
+                function(i, p){
+                  sum(p[-i])
+                }, p=p)
+
+  p*pin/(1-p)
+}
+
 #' Vectorized structured sample
 #'
 #' @param x vector containing sample size (i.e. richness) in the first element
 #' and probabilities on the remaining
 #' @param sp elements to be sampled
 #' @param resu vector of results, must be the same length of sp
-#' @param fr frequency of each sp element. This will be multiplied by the
+#' @param fr_prob Frequency or probability of each sp element. This will be multiplied by the
 #' probability vector
 #' @return vector of sampled sp elements
 #' @author Neander Marcel Heming
 # #' @examples
 #'
 #' @keywords internal
-.str.sample <- function(x, sp, resu, fr){
+.str.sample <- function(x, sp, resu, fr_prob){
   if(is.na(x[1])) {
     resu[] <- NA
   } else {
-    resu[sample(sp, size = x[1], prob = x[-1]*fr)] <- 1
+    resu[sample(sp, size = x[1], prob = x[-1]*fr_prob)] <- 1
   }
   resu
 }
@@ -35,7 +83,7 @@
 #' pattern of each randomized species.
 #' @param rich SpatRaster. Richness pattern structuring the sample size of
 #' each cell randomization. Calculated if not provided.
-#' @param fr frequency of pixels that a species is observed within each layer of x
+#' @param fr_prob Either frequency of pixels or probability that a species is observed within each layer of x
 #' @inheritParams terra::app
 # #' @param cores
 # #' @param filename Character. Filename for output SpatRaster.
@@ -73,17 +121,17 @@
 #' }
 #' @return SpatRaster object
 #' @export
-bootspat_str <- function(x, prob=NULL, rich=NULL, fr=NULL, cores = 1, filename = "", memory = NULL, ...){
+bootspat_str <- function(x, prob=NULL, rich=NULL, fr_prob=NULL, cores = 1, filename = "", memory = NULL, ...){
 
   if(is.null(memory)){
     memory <- .fit.memory(x)
   }
 
-  if(is.null(fr)){
+  if(is.null(fr_prob)){
     # uses utils::globalVariables because of https://github.com/r-lib/devtools/issues/1714
     # utils::globalVariables(value)
     value <- NULL
-    fr <- subset(terra::freq(x), value==1)[,"count"]
+    fr_prob <- subset(terra::freq(x), value==1)[,"count"]
   }
 
   if(is.null(prob)){
@@ -102,7 +150,7 @@ bootspat_str <- function(x, prob=NULL, rich=NULL, fr=NULL, cores = 1, filename =
 
   r <- terra::app(c(rich, prob),
                   .str.sample,
-                  sp=sp, resu=resu, fr=fr,
+                  sp=sp, resu=resu, fr_prob=fr_prob,
   cores = cores, filename = filename, overwrite = T)
 
   terra::set.names(r, names(prob))
