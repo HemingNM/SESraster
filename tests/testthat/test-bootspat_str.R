836e2963b45835005758d6f6d756101233543c23
flaviomoc <flaviomoc@gmail.com>
2023-06-15 16:42:05 -0300

    function coverage increased

--- tests/testthat/test-bootspat_str.R
+++ tests/testthat/test-bootspat_str.R
@@ -6,10 +6,15 @@ test_that("function bootspat_str works", {
   bin1 <- terra::rast(ncol = 5, nrow = 5, nlyr = 5)
   values(bin1) <- round(runif(ncell(bin1) * nlyr(bin1)))
   names(bin1) <- paste0("sp", 1:5)
+  bin2 <- terra::rast(ncol = 5, nrow = 5, nlyr = 1)
+  values(bin2) <- runif(ncell(bin2) * nlyr(bin2))
+  bin1[1] <- NA
 
   # applying the function
   rand.str <- bootspat_str(bin1)
+  rand.str2 <- bootspat_str(bin1, rprob = bin2)
 
   # testing
   expect_true(class(rand.str) == "SpatRaster", "TRUE")
+  expect_true(class(rand.str2) == "SpatRaster", "TRUE")
 })
