bc6e3fba5ceff9f9c1ff2948064bd597ece6db75
Neander Marcel Heming <neanderh@yahoo.com.br>
2023-06-19 14:52:33 -0300

    improved test coverage. Now at 100%

--- tests/testthat/test-bootspat_naive.R
+++ tests/testthat/test-bootspat_naive.R
@@ -1,4 +1,7 @@
 test_that("bootspat_naive throws error when object classes are not appropriate", {
+  # loading data
+  bin1 <- load_ext_data() #terra::rast(system.file("extdata", "spp_sites.tif", package="SESraster"))
+
   expect_error(bootspat_naive(bin1, "x"))
   expect_error(bootspat_naive(bin1, "site", memory = NA))
 })
@@ -13,7 +16,7 @@ test_that("function bootspat_naive works", {
   rand.site <- bootspat_naive(bin1, "site")
   rand.site2 <- bootspat_naive(bin1, "site", memory = F)
   rand.sp <- bootspat_naive(bin1, "species")
-  rand.sp2 <- bootspat_naive(bin1, "species", memory = F)
+  rand.sp2 <- bootspat_naive(bin1, "species", memory = F, filename=paste0(tempfile(), ".tif"))
   rand.both <- bootspat_naive(bin1, "both")
   rand.both2 <- bootspat_naive(bin1, "both", memory = F)
 
