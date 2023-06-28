3bf60fe4d9e2e7087e0aa3ca3cdf15147f3e53ce
Neander Marcel Heming <neanderh@yahoo.com.br>
2023-06-28 12:11:30 -0300

    Added F-F algorithm Working on a new vignette to overview algorithms

---
+++ tests/testthat/test-bootspat_ff.R
@@ -0,0 +1,26 @@
+test_that("function bootspat_FF works", {
+  set.seed(100)
+
+  # loading data
+  bin1 <- load_ext_data() # terra::rast(system.file("extdata", "spp_sites.tif", package="SESraster"))
+  bin1 <- c(bin1, bin1[[1]])
+  bin1[[6]] <- c(NA, rep(1, (terra::ncol(bin1[[6]])*terra::nrow(bin1[[6]]))-1))
+  rprobnull <- terra::app(bin1,
+                          function(x){
+                            ifelse(is.na(x), NA, 1)
+                          })
+
+  # applying the function
+  rand.str <- bootspat_ff(bin1)
+  rand.str2 <- bootspat_ff(bin1, rprob = rprobnull)
+
+  # testing
+  expect_true(inherits(rand.str, "SpatRaster"), "TRUE")
+  expect_true(inherits(rand.str2, "SpatRaster"), "TRUE")
+  expect_equal(unlist(rand.str2[2]), setNames(c(1,1,0,0,0,1), names(bin1)))
+  expect_equal(unlist(terra::global(rand.str2, function(x) sum(x, na.rm = TRUE)))[1:2],
+               unlist(terra::global(bin1, function(x) sum(x, na.rm = TRUE)))[1:2])
+  expect_equal(sum(rand.str)[1:8], sum(bin1)[1:8])
+  expect_equal(sum(rand.str2)[1:8], sum(bin1)[1:8])
+  expect_true(all(is.na(unlist(rand.str2[1]))))
+})
