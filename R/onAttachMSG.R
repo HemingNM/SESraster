aaebc7e8f72b43f61554fbf8acec656de8e73c56
Neander Marcel Heming <neanderh@yahoo.com.br>
2023-07-06 09:47:16 -0300

    fixed bug in attachMSG

--- R/onAttachMSG.R
+++ R/onAttachMSG.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
-  txt <- paste(c(paste("This is SESraster", packageVersion("SESraster")),
+  txt <- paste(c(paste("This is SESraster", utils::packageVersion("SESraster")),
                "To support our work, cite SESraster in your publications. See:",
                '  citation("SESraster")'), collapse="\n")
   packageStartupMessage(txt)
